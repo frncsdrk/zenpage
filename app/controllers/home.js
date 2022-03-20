4d50548de9f9f4c38a0c24c746dc6cd1c013571c
frncsdrk <frncsdrk@protonmail.com>
2022-03-20 20:27:04 +0100

    Fix injection vulnerabilty
    with prop helper

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,9 @@
 import Controller from '@ember/controller';
-import { htmlSafe } from '@ember/template';
 import documentDimensions from 'zenpage/utils/document-dimensions';
 
 export default class HomeController extends Controller {
-  get backgroundImgUrl() {
+  get background() {
     const { height, width } = documentDimensions();
-    return htmlSafe(`https://source.unsplash.com/random/${width}x${height}`);
+    return `background: url(https://source.unsplash.com/random/${width}x${height});`;
   }
 }
