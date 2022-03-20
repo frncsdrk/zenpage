dc1a4c0b535019d998497f4f660946cd2bf2971c
frncsdrk <frncsdrk@protonmail.com>
2022-03-20 12:48:52 +0100

    Add basic localStorage service

--- app/services/local-storage.js
+++ app/services/local-storage.js
@@ -1,3 +1,22 @@
 import Service from '@ember/service';
 
-export default class LocalStorageService extends Service {}
+export default class LocalStorageService extends Service {
+  write(key, value) {
+    localStorage.setItem(key, value);
+    return true;
+  }
+
+  read(key) {
+    return localStorage.getItem(key);
+  }
+
+  delete(key) {
+    localStorage.removeItem(key);
+    return true;
+  }
+
+  clear() {
+    localStorage.clear();
+    return true;
+  }
+}
