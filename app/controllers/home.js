1b6ec331a1a3fecb90f00d1b6a84946cd19b0ab6
frncsdrk <frncsdrk@protonmail.com>
2022-03-26 09:15:00 +0100

    [feat] Add keyboard shortcut to settings route
    - add keyboard shortcut to transition from home to settings route

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,25 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
-export default class HomeController extends Controller {}
+export default class HomeController extends Controller {
+  @service router;
+
+  constructor() {
+    super(...arguments);
+    document.addEventListener('keypress', (event) => {
+      this.onKeypress(this, event);
+    });
+  }
+
+  onKeypress(context, event) {
+    const shortcuts = {
+      s: () => {
+        context.router.transitionTo('settings');
+      },
+    };
+
+    if (shortcuts[event.key]) {
+      shortcuts[event.key]();
+    }
+  }
+}
