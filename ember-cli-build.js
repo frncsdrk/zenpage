35496ca68bf43851422681c968e604f48e5e4fd0
frncsdrk <git@frncsdrk.me>
2023-04-19 01:32:03 +0200

    [style] Configure TailwindCSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,12 +3,35 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    'ember-bootstrap': {
-      bootstrapVersion: 5,
-      importBootstrapCSS: false,
+  const app = new EmberApp(defaults, {});
+
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.css$/i,
+              use: [
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      config: 'postcss.config.js',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
     },
   });
-
-  return app.toTree();
 };
